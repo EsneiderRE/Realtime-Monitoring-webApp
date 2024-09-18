bbbe86bbdf0c2d6330cba895dbb16f10f728958a
EsneiderRE <e.restrepoe@uniandes.edu.co>
2024-09-17 21:42:58 -0500

    Update settings.py

--- IOTMonitoringServer/IOTMonitoringServer/settings.py
+++ IOTMonitoringServer/IOTMonitoringServer/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-bu+)8ft@9+qd*#e#f_s@wkyv2tmq+#!a^3j15h3kjk^jzksu0j
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["localhost", "3.88.113.27"]
+ALLOWED_HOSTS = ["localhost", "3.87.92.178"]
 
 
 # Application definition
@@ -95,7 +95,7 @@ DATABASES = {
         "NAME": "iot_data",  # Nombre de la base de datos
         "USER": "dbadmin",  # Nombre de usuario
         "PASSWORD": "uniandesIOT1234*",  # Contraseña
-        "HOST": "44.223.28.129",  # Dirección IP de la base de datos
+        "HOST": "3.87.92.178",  # Dirección IP de la base de datos
         "PORT": "",  # Puerto de la base de datos
     }
 }
